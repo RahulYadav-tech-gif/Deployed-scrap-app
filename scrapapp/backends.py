4c7919e702f6888242d1104e6148ce4ff8defc14
Rahul Yadav <iamrahulgurjar99@gmail.com>
2024-11-04 15:34:27 +0530

    commmit

--- scrapapp/backends.py
+++ scrapapp/backends.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+from django.core.exceptions import MultipleObjectsReturned
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
@@ -8,6 +9,8 @@ class EmailBackend(ModelBackend):
             user = UserModel.objects.get(email=email)
         except UserModel.DoesNotExist:
             return None
+        except MultipleObjectsReturned:
+            return None  
         if user.check_password(password):
             return user
         return None
